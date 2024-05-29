ed2af88976a9ab5e49b14a53c6852c1b1a6f3c58
olol85 <olol85@gmail.com>
2024-05-29 12:45:23 +0200

    grid layout

--- handlers/home.go
+++ handlers/home.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"gothstarter/views/home"
 	"net/http"
 )
@@ -8,5 +9,9 @@ import (
 var items = []string{"item1", "item2", "item3"}
 
 func HandleHome(w http.ResponseWriter, r *http.Request) error {
+	var items = []string{}
+	for i := range 80 {
+		items = append(items, fmt.Sprintf("item %v", i))
+	}
 	return Render(w, r, home.Index(items))
 }
